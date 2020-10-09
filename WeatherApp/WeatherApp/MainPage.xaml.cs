482d7248d6603f5c4809654fe087f5149aed3df1
gsubram <g.n.subramanian@gmail.com>
2020-10-08 23:07:03 -0400

    Completed UI and API Call

--- WeatherApp/WeatherApp/MainPage.xaml.cs
+++ WeatherApp/WeatherApp/MainPage.xaml.cs
@@ -1,18 +1,33 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xamarin.Forms;
 
 namespace WeatherApp
 {
     public partial class MainPage : ContentPage
     {
+        RestService _restService;
         public MainPage()
         {
             InitializeComponent();
+            _restService = new RestService();
+        }
+
+        async void OnButtonClicked(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(cityEntry.Text))
+            {
+                WeatherData weatherData = await _restService.GetWeatherDataAsync(GenerateRequestUri(Constants.OpenWeatherMapEndpoint));
+                BindingContext = weatherData;
+            }
+        }
+
+        string GenerateRequestUri(string endpoint)
+        {
+            string requestUri = endpoint;
+            requestUri += $"?q={cityEntry.Text}";
+            requestUri += $"&units=imperial"; //or units=metric
+            requestUri += $"&APPID={Constants.OpenWeatherMapAPIKey}";
+            return requestUri;
         }
     }
 }
